65104cd16210dae4534273d7fcb55945a5d3630d
Kgosi Motlogelwa <komotlogelwa@gmail.com>
2021-12-14 16:46:19 +0200

    got material ui working in the project

--- marioplan/src/App.js
+++ marioplan/src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
-import { Grid, Item, Menu } from "./components/muiCollection/indexMUI";
+import { Grid, Menu } from "./components/muiCollection/indexMUI";
 
 function App() {
   return (
@@ -8,17 +8,21 @@ function App() {
       <div className="App">
         {/* <Navbar /> */}
         <Grid container spacing={2}>
-          <Grid item xs={6} md={8}>
-            <Item>xs=6 md=8</Item>
+          <Grid item xs={6} md={8} style={{ border: "1px solid black " }}>
+            Grid One
+            {/* <Item>xs=6 md=8</Item> */}
           </Grid>
-          <Grid item xs={6} md={4}>
-            <Item>xs=6 md=4</Item>
+          <Grid item xs={6} md={4} style={{ border: "1px solid black " }}>
+            Grid Two
+            {/* <Item>xs=6 md=4</Item> */}
           </Grid>
-          <Grid item xs={6} md={4}>
-            <Item>xs=6 md=4</Item>
+          <Grid item xs={6} md={4} style={{ border: "1px solid black " }}>
+            Grid Three
+            {/* <Item>xs=6 md=4</Item> */}
           </Grid>
-          <Grid item xs={6} md={8}>
-            <Item>xs=6 md=8</Item>
+          <Grid item xs={6} md={8} style={{ border: "1px solid black " }}>
+            sdfasd
+            {/* <Item>xs=6 md=8</Item> */}
           </Grid>
         </Grid>
       </div>
--- marioplan/src/components/muiCollection/indexMUI.js
+++ marioplan/src/components/muiCollection/indexMUI.js
@@ -1,7 +1,8 @@
 import React from "react";
 
-export { Button } from "@material-ui/core/Button";
-export { Card } from "@material-ui/core/Card";
-export { Grid } from "@material-ui/core/Grid";
-export { Item } from "@material-ui/core/Item";
-export { Menu } from "@material-ui/core/Menu";
+import Button from "@mui/material/Button";
+import Card from "@mui/material/Card";
+import Grid from "@mui/material/Grid";
+import Menu from "@mui/material/Menu";
+
+export { Button, Card, Grid, Menu };
